dd1bd226f293f9d6264b6e6dfd12d22a14172c29
brocode <kelimbth@gmail.com>
2023-05-01 17:38:58 +0300

    added cards to display country and their respective flags

--- lib/screens/location.dart
+++ lib/screens/location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class Location extends StatefulWidget {
   const Location({Key? key}) : super(key: key);
@@ -8,11 +9,16 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
+  List<WorldTime> locations = [
+    WorldTime(location: 'Cairo', countryFlag: 'egypt.png', timezone: 'Africa/Cairo'),
+    WorldTime(location: 'Nairobi', countryFlag: 'kenya.png', timezone: 'Africa/Nairobi'),
+    WorldTime(location: 'Berlin', countryFlag: 'germany.png', timezone: 'Europe/Berlin'),
+    WorldTime(location: 'London', countryFlag: 'uk.png', timezone: 'Europe/London'),
+    WorldTime(location: 'Seoul', countryFlag: 'south-korea.png', timezone: 'Asia/Seoul'),
+    WorldTime(location: 'Tokyo', countryFlag: 'japan.png', timezone: 'Asia/Tokyo'),
+    WorldTime(location: 'Toronto', countryFlag: 'canada.png', timezone: 'America/Toronto'),
+    WorldTime(location: 'New York', countryFlag: 'us.png', timezone: 'America/New_York'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +28,25 @@ class _LocationState extends State<Location> {
         centerTitle: true,
         backgroundColor: Colors.indigo,
       ),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {},
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/flag-icons/${locations[index].countryFlag}'),
+
+                  ),
+                ),
+              ),
+            );
+          },
+      ),
+
     );
   }
 }
