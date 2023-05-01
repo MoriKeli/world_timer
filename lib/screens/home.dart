2db18ff5e00163dfc5ed28d4668ef0a41589dd24
brocode <kelimbth@gmail.com>
2023-05-01 17:07:55 +0300

    updated UI -> added bg color and bg images

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,40 +14,57 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     dynamic data = ModalRoute.of(context)?.settings.arguments;
 
+    // set background image
+    String bgImage = data['isDayTime'] ? 'day.jpg' : 'night.jpg';
+    Color textColor = data['isDayTime'] ? Colors.black :  Colors.white;
+    Color bgColor = data['isDayTime'] ? Colors.lightBlue :  Colors.indigo;
+
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget> [
-              ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
-                  },
-                  icon: const Icon(Icons.location_on),
-                label: const Text('Edit location'),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget> [
-                  Text(
-                      data['location'],
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        letterSpacing: 2.0,
-                      ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 60.0,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/$bgImage'), fit: BoxFit.cover),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget> [
+                ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: const Icon(Icons.location_on),
+                  label: const Text('Edit location'),
                 ),
-              ),
-            ],
+                const SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget> [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 75.0, 0, 0),
+                      child: Text(
+                          data['location'],
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            letterSpacing: 2.0,
+                            color: textColor,
+                          ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 60.0,
+                    color: textColor,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       )
